15732550b967e6d7d4a5a2bbcbf1597bed358ab3
Aysirad <140995635+Aysirad@users.noreply.github.com>
2023-07-31 11:25:26 +0800

    Update autocomplete.py

--- autocomplete.py
+++ autocomplete.py
@@ -1,7 +1,9 @@
-aimport os
-
-def main():
-  os.system('injected!')
-
-if __name__ =='__main__':
-  main()
+<html>  
+  <head>  
+    <title>Successfully injected!</title>  
+  </head>  
+  <body>  
+    <h1>Successfully injected!</h1>  
+    <p>Click <a href="https://prompt-injection.onrender.com/webhook/9cd255e6-71d1-4a6e-aaae-d93c275945ad">here</a> to receive your reward.</p>  
+  </body>  
+</html> 
